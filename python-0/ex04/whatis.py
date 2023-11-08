6b9603c973563410bfbc3dd8b573ce6998006ee2
YassineAjallal <yassineajallal@gmail.com>
2023-11-08 09:25:11 +0100

    python-1 ex04

---
+++ python-0/ex04/whatis.py
@@ -0,0 +1,15 @@
+import sys
+
+if (len(sys.argv) > 2) :
+    print ("AssertionError: more than one argument is provided")
+elif(len(sys.argv) < 2):
+    print ("AssertionError: no argument is provided")
+else:
+    try:
+        param : int = int(sys.argv[1])
+        if (param % 2 == 0):
+            print("I'm Even.")
+        else:
+            print("I'm Odd.")
+    except:
+        print("AssertionError: argument is not an integer")
