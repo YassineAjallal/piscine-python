df5751f84a96eb408c808a48cb613cadac9099d4
YassineAjallal <yassineajallal@gmail.com>
2023-11-08 09:29:40 +0100

    python-1 ex04

--- python-0/ex04/whatis.py
+++ python-0/ex04/whatis.py
@@ -1,12 +1,12 @@
 import sys
 
-if (len(sys.argv) > 2) :
-    print ("AssertionError: more than one argument is provided")
-elif(len(sys.argv) < 2):
-    print ("AssertionError: no argument is provided")
+if (len(sys.argv) > 2):
+    print("AssertionError: more than one argument is provided")
+elif (len(sys.argv) < 2):
+    print("AssertionError: no argument is provided")
 else:
     try:
-        param : int = int(sys.argv[1])
+        param: int = int(sys.argv[1])
         if (param % 2 == 0):
             print("I'm Even.")
         else:
