fad151c69fb5d04a5550ba049cef0e60832377bf
YassineAjallal <yassineajallal@gmail.com>
2023-11-13 16:10:56 +0100

    python-django librairies ex05

--- python-django/librairies/ex01/test.py
+++ python-django/librairies/ex01/test.py
@@ -1,21 +1,21 @@
-from local_lib import path
+# from local_lib import path
 
 
-def create_folder() -> None:
-    try:
-        path.os.mkdir('test_folder')
-    except FileExistsError:
-        print('test_folder: already exist')
-    try:
-        f = open(path.os.getcwd() + '/test_folder/test_file', 'w+')
-        f.write("hello from path")
-        f.seek(0)
-        lines = f.readlines()
-        for line in lines:
-            print(line)
-    except Exception as err:
-        print(err)
+# def create_folder() -> None:
+#     try:
+#         path.os.mkdir('test_folder')
+#     except FileExistsError:
+#         print('test_folder: already exist')
+#     try:
+#         f = open(path.os.getcwd() + '/test_folder/test_file', 'w+')
+#         f.write("hello from path")
+#         f.seek(0)
+#         lines = f.readlines()
+#         for line in lines:
+#             print(line)
+#     except Exception as err:
+#         print(err)
 
 
-if __name__ == "__main__":
-    create_folder()
+# if __name__ == "__main__":
+#     create_folder()
