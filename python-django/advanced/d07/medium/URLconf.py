c97bc3c85ec1d8eaedcfbeb85aa847bd31e50b29
YassineAjallal <yassineajallal@gmail.com>
2023-12-19 19:08:01 +0100

    advanced-django ex00 ex01

--- python-django/advanced/d07/medium/URLconf.py
+++ python-django/advanced/d07/medium/URLconf.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import Articles, Home
+from .views import Articles, Home, Login, Publications, Details
 urlpatterns = [
     path('', Home.as_view(), name='home_view'),
-    path('articles/', Articles.as_view(), name='articles_view')
+    path('articles/', Articles.as_view(), name='articles_view'),
+    path('publications/', Publications.as_view(), name='publications_view'),
+    path('details/<int:pk>/', Details.as_view(), name='details_view'),
+    path('login/', Login.as_view(), name='login_view')
 ]
--- python-django/advanced/d07/medium/views.py
+++ python-django/advanced/d07/medium/views.py
@@ -1,8 +1,12 @@
 from typing import Any
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse, HttpResponse as HttpResponse
+from django.shortcuts import redirect
 from django.views.generic.base import TemplateView, RedirectView
+from django.views.generic.edit import FormView
+from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout
 from .models import Article
-from django.contrib.auth.models import User
 
 class Articles(TemplateView):
     template_name = 'articles.html'
@@ -10,7 +14,12 @@ class Articles(TemplateView):
         context = super().get_context_data(**kwargs)
         context['articles'] = Article.objects.all()
         return context
-    
+    def dispatch(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        if self.request.method == 'POST':
+            if 'action' in self.request.POST and self.request.POST['action'] == 'logout':
+                logout(request)
+                return redirect('login_view')
+        return super().dispatch(request, *args, **kwargs)
 
 class Home(RedirectView):
     pattern_name = 'articles_view'
@@ -19,4 +28,47 @@ class Home(RedirectView):
     def get_redirect_url(self, *args: Any, **kwargs: Any):
         return super().get_redirect_url(*args, **kwargs)
     
-class Login():
+class Login(FormView):
+    template_name = 'login.html'
+    form_class = AuthenticationForm
+    success_url = '/'
+
+    def get(self, request: HttpRequest, *args: str, **kwargs: Any) -> HttpResponse:
+        if request.user.is_authenticated:
+            return redirect('home_view')
+        return super().get(request, *args, **kwargs)
+    def form_valid(self, form: Any) -> HttpResponse:
+        user = form.get_user()
+        login(self.request, user)
+        return super().form_valid(form)
+    
+class Publications(TemplateView):
+    template_name = 'publications.html'
+    def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        if not request.user.is_authenticated:
+            return redirect('login_view')
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context =  super().get_context_data(**kwargs)
+        print(self.request.user)
+        context['publications'] = Article.objects.filter(author__username=str(self.request.user))
+        return context
+    def dispatch(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        if self.request.method == 'POST':
+            if 'action' in self.request.POST and self.request.POST['action'] == 'logout':
+                logout(request)
+                return redirect('login_view')
+        return super().dispatch(request, *args, **kwargs)
+    
+class Details(DetailView):
+    model = Article
+    template_name='details.html'
+    context_object_name = 'article'
+    def dispatch(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        if self.request.method == 'POST':
+            if 'action' in self.request.POST and self.request.POST['action'] == 'logout':
+                logout(request)
+                return redirect('login_view')
+        return super().dispatch(request, *args, **kwargs)
+    
