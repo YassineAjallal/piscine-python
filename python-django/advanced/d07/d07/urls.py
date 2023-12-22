b2ce5a2588f9198b603d734c6dd43e8f491a63fa
YassineAjallal <yassineajallal@gmail.com>
2023-12-22 11:47:44 +0100

    advanced-django ex04

--- python-django/advanced/d07/d07/urls.py
+++ python-django/advanced/d07/d07/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
+from django.utils.translation import gettext_lazy as _
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('medium.URLconf'))
-]
+urlpatterns = i18n_patterns(
+    path(_('admin/'), admin.site.urls),
+    path('', include('medium.URLconf')),
+    path('rosetta/', include('rosetta.urls'))
+)
